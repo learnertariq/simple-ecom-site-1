eb250da98ad395461bfc2da6d104ec41e1906b55
Tariqul Islam <usukuk@gmail.com>
2022-07-05 08:13:01 +0600

    make responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,18 +3,19 @@ import Cart from "../components/Cart";
 import Layout from "../components/Layout";
 import Product from "../components/Product";
 import data from "../database/data";
-import { Store } from "../utils/Store";
 
 export default function Home({ products }) {
   return (
     <Layout title="Homepage">
-      <section className="container mx-auto grid grid-cols-5 gap-4">
-        <section className="products col-span-3 grid grid-cols-2 gap-8">
-          {products.map((p) => (
-            <Product key={p.id} p={p} />
-          ))}
+      <section className="container mx-auto grid grid-cols-1 lg:grid-cols-5 gap-4">
+        <section className="products-outer col-span-3 order-last lg:order-first">
+          <div className="products grid sm:grid-cols-2 gap-8">
+            {products.map((p) => (
+              <Product key={p.id} p={p} />
+            ))}
+          </div>
         </section>
-        <aside className="cart col-span-2 px-4">
+        <aside className="cart lg:col-span-2 px-4">
           <Cart />
         </aside>
       </section>
